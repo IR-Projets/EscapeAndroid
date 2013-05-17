00473e2eed471acde462d0c57409601c0dc08cbb
kiuby <quentin.bernard@univ-paris-est.fr>
2013-05-17 19:37:11 +0200

    maj layout principal et ecran levels

--- src/editor/LevelsActivity.java
+++ src/editor/LevelsActivity.java
@@ -1,12 +1,15 @@
 package editor;
 
+import game.Environnement;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.example.escapeandroid.R;
@@ -14,80 +17,85 @@ import com.example.escapeandroid.R;
 public class LevelsActivity extends Activity{
 
 	private int nbLevel = 0;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_levels);
-		
+
 		ImageView image_addButton = (ImageView)findViewById(R.id.add_button);
 		image_addButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				addLevel(v);
-				
-			}
-		});
-		
-		ImageView image_subButton = (ImageView)findViewById(R.id.sub_button);
-		image_subButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				removeLevel(v);
-				
-			}
-		});
-		
-		
-	}
-	
-	
-	private void addLevel(View v){
-		if(nbLevel > 6 )
-			return;
-		
-		nbLevel++;
-		
-		Button myButton = new Button(this);
-		myButton.setText("Level "+nbLevel);
-		myButton.setId(nbLevel);
-		
-		myButton.setWidth(v.getWidth());
-		myButton.setX(v.getWidth()/2);
-		myButton.setY((nbLevel+3)*v.getHeight());
-		
-		
-		final LevelsActivity levelsActivity = this;
-		myButton.setOnClickListener(new OnClickListener() {
-		
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(levelsActivity, EditorActivity.class);
-				startActivity(intent);
+
 			}
 		});
 
-		RelativeLayout rl = (RelativeLayout)findViewById(R.id.levels_layout_top);
-		/*LayoutParams lp = new LayoutParams(rl.getLayoutParams());
-		lp.addRule(RelativeLayout.CENTER_IN_PARENT);*/
-		rl.addView(myButton);
+		ImageView image_subButton = (ImageView)findViewById(R.id.sub_button);
+		image_subButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				removeLevel(v);
+
+			}
+		});
+
+
+	}
+
+	public void click(View v){
+		Button myButton = (Button) v;
+		Intent intent = new Intent(this, EditorActivity.class);
+		startActivity(intent);
+	}
+
+	private void addLevel(View v){
+		if(nbLevel > 4 )
+			return;
+
+		nbLevel++;
+
+		Button myButton = getButtonByLevel();
+
+		myButton.setText("Level "+(nbLevel+1));
+		myButton.setVisibility(View.VISIBLE);
 		
 	}
-	
+
 	private void removeLevel(View v){
 		if(nbLevel < 1)
 			return;
 		
-		
-		RelativeLayout rl = (RelativeLayout)findViewById(R.id.levels_layout_top);
-		Button buttonToRemove = (Button)findViewById(nbLevel);
-		rl.removeView(buttonToRemove);
+		Button myButton = getButtonByLevel();
+		myButton.setVisibility(View.INVISIBLE);
+
 		nbLevel--;
 	}
-	
-	
-	
+
+	private Button getButtonByLevel() {
+		Button myButton = null;
+		switch(nbLevel){
+		case 1 :
+			myButton = (Button)findViewById(R.id.level2);
+			break;
+		case 2 :
+			myButton = (Button)findViewById(R.id.level3);
+			break;
+		case 3 :
+			myButton = (Button)findViewById(R.id.level4);
+			break;
+		case 4 :
+			myButton = (Button)findViewById(R.id.level5);
+			break;
+		case 5 :
+			myButton = (Button)findViewById(R.id.level6);
+			break;
+		}
+		return myButton;
+	}
+
 }
